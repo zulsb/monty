5ac068ff5a64044b812257a967367395209b78a7
Luz <zulsb2093@gmail.com>
2020-01-06 03:58:05 +0000

    Pop

---
+++ _pop.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+  *_pop - Removes the top element of the stack.
+  *@stack: Double pointer stack.
+  *@line_number: Number in file.
+  */
+
+void _pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tm;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		printf("L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	tm = *stack;
+	*stack = (*stack)->next;
+
+	free(tm);
+}
