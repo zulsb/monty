59bcddb9b0e31b28a1609217d2f29926cab65759
Luz <zulsb2093@gmail.com>
2020-01-06 04:00:17 +0000

    Header

--- monty.h
+++ monty.h
@@ -61,9 +61,9 @@ void parse(stack_t **stack, char *op, unsigned int line_num);
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
-void swap(stack_t **stack, unsigned int line_number);
-void add(stack_t **stack, unsigned int line_number);
-void nop(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
 
 #endif
