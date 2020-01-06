ac0f1a43160981d5adc334ea0b27a595a914f897
Luz <zulsb2093@gmail.com>
2020-01-06 04:44:54 +0000

    Nop

---
+++ _nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+  *_nop - Opcode.
+  *@stack: Double pointer stack.
+  *@line_number: Number in file.
+  */
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
