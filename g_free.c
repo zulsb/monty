b91bc873283d5744d77f8ebae19ef45e14e73618
Luz <zulsb2093@gmail.com>
2020-01-06 03:13:28 +0000

    Free

---
+++ g_free.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+  *glo_f - Free memory variables globals.
+  *
+  */
+
+void glo_f(void)
+{
+	stack_t *t;
+	stack_t *tmp;
+
+	tmp = NULL;
+	t = *glo;
+
+	while (t)
+	{
+		tmp = t->next;
+		free(t);
+		t = tmp;
+	}
+
+}
