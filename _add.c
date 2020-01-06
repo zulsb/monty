16a04ba47c8f4030603c4e191731a86fb0838897
Luz <zulsb2093@gmail.com>
2020-01-06 04:45:33 +0000

    Add

---
+++ _add.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+  *_add - Adds the top two elements of the stack.
+  *@stack: Double pointer stack.
+  *@line_number: Number in file.
+  *
+  */
+
+void _add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tm;
+	int sm;
+
+	if (!stack || !(*stack) || !(*stack)->next)
+	{
+		printf("L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	tm = *stack;
+
+	sm = tm->n + tm->next->n;
+	tm->next->n = sm;
+
+	*stack = tm->next;
+
+	free(tm);
+}
