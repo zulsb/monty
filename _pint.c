71a33373dd549b23801f46f2e7b1a10981959126
Luz <zulsb2093@gmail.com>
2020-01-07 00:52:32 +0000

    Pint

--- _pint.c
+++ _pint.c
@@ -12,6 +12,7 @@ void _pint(stack_t **stack, unsigned int line_number)
 	{
 		printf("L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
+		return;
 	}
 
 	printf("%d\n", (*stack)->n);
